0f955d52abde4016193955b7016a60a34b3ae298
Alan Felix <felixnascc@gmail.com>
2022-07-12 14:50:32 -0300

    Adição de roteamento aninhado e tema padrão

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import PaginaPadrao from 'components/PaginaPadrao';
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
@@ -5,12 +6,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
