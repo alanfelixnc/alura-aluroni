e8040c91b737a9d1650a33629389bc0edfd7ebcf
Alan Felix <felixnascc@gmail.com>
2022-07-18 19:32:51 -0300

    Criação do botão de voltar na tela de Not Found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,8 +2,11 @@ import styles from './NotFound.module.scss';
 import theme from 'styles/Tema.module.scss';
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <div
       className={classNames({
@@ -11,6 +14,9 @@ export default function NotFound() {
         [theme.container]: true,
       })}
     >
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
+      </div>
       <NotFoundImage />
     </div>
   );
